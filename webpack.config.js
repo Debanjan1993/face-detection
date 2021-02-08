f872dad4737bc3c927c37b731d4b15c8538992b0
debanjanBQ <debanjan@bitqit.com>
2021-02-09 03:47:49 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/script.ts',
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            include: [path.resolve(__dirname, './src')]
+        }]
+    },
+    output: {
+        publicPath: './public',
+        filename: 'script.js',
+        path: path.resolve(__dirname, './public')
+    }
+}
