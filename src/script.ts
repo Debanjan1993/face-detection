9b129872c7fbb794197701ca9f3e85fe0ca819ed
debanjanBQ <debanjan@bitqit.com>
2021-08-12 21:24:31 +0530

    added error handling

--- src/script.ts
+++ src/script.ts
@@ -8,7 +8,7 @@ Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
     faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(() => startVideo());
+]).then(() => startVideo()).catch(error => console.error(error));
 
 
 const startVideo = () => {
@@ -29,19 +29,19 @@ video.addEventListener('play', () => {
         width: video.width,
         height: video.height
     }
-    faceapi.matchDimensions(canvas,displaySize);
+    faceapi.matchDimensions(canvas, displaySize);
 
     setInterval(async () => {
-        const detections =await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-            .withFaceLandmarks().withFaceExpressions().withAgeAndGender();    
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks().withFaceExpressions().withAgeAndGender();
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas,resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
         resizedDetections.forEach(detection => {
             const box = detection.detection.box;
-            const drawBox = new faceapi.draw.DrawBox(box, {label : `${Math.round(detection.age)} years old ${detection.gender}`})
+            const drawBox = new faceapi.draw.DrawBox(box, { label: `${Math.round(detection.age)} years old ${detection.gender}` })
             drawBox.draw(canvas);
         })
     }, 100);
